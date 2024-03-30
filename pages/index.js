3f59e4def33af5e609750e9afc29c60561dae34f
Rogerio Brito <rogeriobrito84@gmail.com>
2024-03-30 10:20:33 +0000

    feat: adicionando chokito.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Eu amo minha esposa (Claudiana) e minha filha (Sophie)!</h1>;
+  return (
+    <h1>Curso Felipe Deschamps</h1>
+    <h2>
+      Eu amo minha esposa (Claudiana), minha filha (Sophie) e meu Pet(chokito)!
+    </h2>
+  );
 }
 
 export default Home;
