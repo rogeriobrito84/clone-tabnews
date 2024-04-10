bc12e58d442acd1634efc10ae6eaba797d270dfd
rogeriobrito84 <rogeriobrito84@gmail.com>
2024-04-10 20:39:12 -0300

    finish `/api/status`endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
-import database from "infra/database.js";
+import db from "infra/database.js";
 
 async function status(req, res) {
-    const result = await database.query("select 1 + 1;");
-    console.log(result);
-    res.status(200).json("Olá Rogério");
+    const updateAt = new Date().toISOString();
+    const searchVersion = await db.query("show server_version;");
+    const versionDb = searchVersion.rows[0].server_version;
+    const searchMaxConnections = await db.query("show max_connections;");
+    const maxConnections = searchMaxConnections.rows[0].max_connections;
+    const databaseName = process.env.POSTGRES_DB;
+    const searchConnectionsOpened = await db.query({
+        text: `select count(*)::int from pg_stat_activity where datname = $1;`,
+        values: [databaseName],
+    });
+    const connectionsOpened = searchConnectionsOpened.rows[0].count;
+
+    res.status(200).json({
+        update_at: updateAt,
+        dependencies: {
+            database: {
+                version: versionDb,
+                max_connections: parseInt(maxConnections),
+                opened_connections: connectionsOpened,
+            },
+        },
+    });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
+const baeUrl = "http://localhost:3000/api/v1/status";
+
 test("Get to /api/v1/status should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
+    const response = await fetch(baeUrl);
+    const body = await response.json();
+    const date = new Date(body.update_at).toISOString();
+    console.log(body);
+
     expect(response.status).toBe(200);
+    expect(body.update_at).toBeDefined();
+    expect(body.update_at).toEqual(date);
+    expect(body.dependencies.database.version).toEqual("16.0");
+    expect(body.dependencies.database.max_connections).toEqual(100);
+    expect(body.dependencies.database.opened_connections).toEqual(1);
 });
