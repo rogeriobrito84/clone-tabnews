252620bce410d9cf497742dc4cea5a8cc0d1d0c1
rogeriobrito84 <rogeriobrito84@gmail.com>
2024-04-09 22:02:50 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+    const result = await database.query("select 1 + 1;");
+    console.log(result);
     res.status(200).json("Olá Rogério");
 }
 
